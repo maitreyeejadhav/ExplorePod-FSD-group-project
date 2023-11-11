04846caaa2a8c532294a2289f10651f11eee16ae
Mansi Patil <86612618+Codermansi@users.noreply.github.com>
2023-11-11 11:35:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Post from './Post';
+import Header from './Header';
+import {Route, Routes} from "react-router-dom";
+import Layout from './Layout';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/login" element={<LoginPage/>} />
+
+      </Route>
+      
+    </Routes>
+   
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+export default function Header()
+{
+    return(
+        <header>
+        <Link to="/" className="logo">Travelcations</Link>
+        <nav>
+          <Link to="/login">Login</Link>
+          <Link to="">Register</Link>
+        </nav>
+      </header> 
+    );
+}
