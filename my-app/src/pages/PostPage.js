2d77d80f1568aaec60a06e39565f760525b331fb
maitreyeejadhav <maitree@gmail.com>
2023-12-01 12:09:52 +0530

    fixed star alignment

--- my-app/src/pages/PostPage.js
+++ my-app/src/pages/PostPage.js
@@ -1,79 +1,86 @@
-import {useContext, useEffect, useState} from "react";
-import {Link, useParams} from "react-router-dom";
-import {formatISO9075} from "date-fns";
-import {UserContext} from "../UserContext";
+import { useContext, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { formatISO9075 } from "date-fns";
+import { UserContext } from "../UserContext";
 
 export default function PostPage() {
-	const [postInfo, setPostInfo] = useState(null);
-	const {userInfo} = useContext(UserContext);
-	const {id} = useParams();
-	useEffect(() => {
-		fetch(`http://localhost:4000/post/${id}`).then((response) => {
-			response.json().then((postInfo) => {
-				setPostInfo(postInfo);
-			});
-		});
-	}, []);
-	
-	if (!postInfo) return "";
-	
-	return (
-		<div className="post-page">
-			<h1>{postInfo.title}</h1>
-			<time>{formatISO9075(new Date(postInfo.createdAt))}</time>
-			<div className="author">by @{postInfo.author.username}</div>
-			<div style={
-				{
-					display: "flex",
-					justifyContent: "center",
-					padding: "5px",
-				}
-			}>
-				<div style={
-					{
-						display: "flex",
-						marginLeft: "5px",
-						gap: "2px",
-						color: "orange",
-					}
-				}>
-					{postInfo.rating ? postInfo.rating : <div>
-						Unrated</div>}
-					<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24">
-						<path fill="currentColor"
-						      d="m12 18.275l-4.15 2.5q-.275.175-.575.15t-.525-.2q-.225-.175-.35-.438t-.05-.587l1.1-4.725L3.775 11.8q-.25-.225-.312-.513t.037-.562q.1-.275.3-.45t.55-.225l4.85-.425l1.875-4.45q.125-.3.388-.45t.537-.15q.275 0 .537.15t.388.45l1.875 4.45l4.85.425q.35.05.55.225t.3.45q.1.275.038.563t-.313.512l-3.675 3.175l1.1 4.725q.075.325-.05.588t-.35.437q-.225.175-.525.2t-.575-.15l-4.15-2.5Z"/>
-					</svg>
-				</div>
-			</div>
-			
-			{userInfo.id === postInfo.author._id && (
-				<div className="edit-row">
-					<Link className="edit-btn" to={`/edit/${postInfo._id}`}>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth={1.5}
-							stroke="currentColor"
-							className="w-6 h-6"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-							/>
-						</svg>
-						Edit this post
-					</Link>
-				</div>
-			)}
-			<div className="image">
-				<img src={`http://localhost:4000/${postInfo.cover}`} alt=""/>
-			</div>
-			<div
-				className="content"
-				dangerouslySetInnerHTML={{__html: postInfo.content}}
-			/>
-		</div>
-	);
+  const [postInfo, setPostInfo] = useState(null);
+  const { userInfo } = useContext(UserContext);
+  const { id } = useParams();
+  useEffect(() => {
+    fetch(`http://localhost:4000/post/${id}`).then((response) => {
+      response.json().then((postInfo) => {
+        setPostInfo(postInfo);
+      });
+    });
+  }, []);
+
+  if (!postInfo) return "";
+
+  return (
+    <div className="post-page">
+      <h1>{postInfo.title}</h1>
+      <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+      <div className="author">by @{postInfo.author.username}</div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "5px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            marginLeft: "5px",
+            gap: "2px",
+            color: "orange",
+          }}
+        >
+          {postInfo.rating ? postInfo.rating : <div>Unrated</div>}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="12"
+            height="12"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="m12 18.275l-4.15 2.5q-.275.175-.575.15t-.525-.2q-.225-.175-.35-.438t-.05-.587l1.1-4.725L3.775 11.8q-.25-.225-.312-.513t.037-.562q.1-.275.3-.45t.55-.225l4.85-.425l1.875-4.45q.125-.3.388-.45t.537-.15q.275 0 .537.15t.388.45l1.875 4.45l4.85.425q.35.05.55.225t.3.45q.1.275.038.563t-.313.512l-3.675 3.175l1.1 4.725q.075.325-.05.588t-.35.437q-.225.175-.525.2t-.575-.15l-4.15-2.5Z"
+            />
+          </svg>
+        </div>
+      </div>
+
+      {userInfo.id === postInfo.author._id && (
+        <div className="edit-row">
+          <Link className="edit-btn" to={`/edit/${postInfo._id}`}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+              />
+            </svg>
+            Edit this post
+          </Link>
+        </div>
+      )}
+      <div className="image">
+        <img src={`http://localhost:4000/${postInfo.cover}`} alt="" />
+      </div>
+      <div
+        className="content"
+        dangerouslySetInnerHTML={{ __html: postInfo.content }}
+      />
+    </div>
+  );
 }
